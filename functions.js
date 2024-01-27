f3063366ce7742bea64f5657d4931af8ee5fa933
Victor Carmona <victor181851@gmail.com>
2024-01-27 14:01:47 -0600

    feat: getUser function was created

--- functions.js
+++ functions.js
@@ -1,54 +1,67 @@
 const users = ["Juan", "Pedro", "Sofia", "Victor"];
 
-
 function sendResponse(code, body = null) {
-  const response = {
-    code,
-    body,
-  };
+    const response = {
+        code,
+        body,
+    };
 
+    switch (code) {
+        case 200:
+            response.msg = "Ok";
+            break;
+        case 400:
+            response.msg = "Endpoint not valid";
+            break;
+        case 404:
+            response.msg = "Not found";
+            break;
+        case 500:
+            response.msg = "Internal Server Error";
+            break;
+        default:
+            response.msg = "Unknown status code";
+    }
 
-  switch (code) {
-    case 200:
-      response.msg = "Ok";
-      break;
-    case 400:
-      response.msg = "Endpoint not valid";
-      break;
-    case 404:
-      response.msg = "Not found";
-      break;
-    case 500:
-      response.msg = "Internal Server Error";
-      break;
-    default:
-      response.msg = "Unknown status code";
-  }
-
-  return response;
+    return response;
 }
 
 function getUser(userName) {
-  try {
+    try {
+        // Early return guard clauses
+        if (!userName) {
+            return sendResponse(400);
+        }
 
-    // Early return guard clauses
-    if (!userName) {
-      return sendResponse(400);
+        const userIndex = users.indexOf(userName);
+
+        if (userIndex >= 0) {
+            const user = users.at(userIndex);
+
+            return sendResponse(200, user);
+        }
+
+        return sendResponse(404);
+    } catch (error) {
+        return sendResponse(500, error);
     }
+}
 
-    const userIndex = users.indexOf(userName);
 
-    if (userIndex >= 0) {
-      const user = users.at(userIndex);
-
-      return sendResponse(200, user);
+function getUsers() {
+    try {
+        if (users.length > 0) {
+            return sendResponse(200, users);
+        } else {
+            return sendResponse(404);
+        }
+    } catch (error) {
+        return sendResponse(500, error);
     }
+}
 
-    return sendResponse(404);
-  } catch (error) {
-    return sendResponse(500, error);
-  }
-};
+
+
 
 
 
