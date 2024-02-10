03da079be1a42c7697fe9899c63d90e059f16eab
Victor Carmona <victor181851@gmail.com>
2024-02-09 20:57:53 -0600

    feat: sendResponse function was created with the possible response codes

--- array_methods_h2_api.js
+++ array_methods_h2_api.js
@@ -180,3 +180,32 @@ const books = [
         "publisher": "Farrar, Straus and Giroux"
     }
 ]
+
+function sendResponse(code, body = null) {
+    const response = {
+        code,
+        body,
+    };
+
+    switch (code) {
+        case 200:
+            response.msg = "Ok";
+            break;
+        case 400:
+            response.msg = "Endpoint not valid";
+            break;
+        case 404:
+            response.msg = "Not found";
+            break;
+        case 500:
+            response.msg = "Internal Server Error";
+            break;
+        case 204:
+            response.msg = "No content";
+            break;
+        default:
+            response.msg = "Unknown status code";
+    }
+
+    return response;
+}
